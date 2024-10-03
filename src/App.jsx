b10085b5bddd2ea9b7d6b5bb61664c6f4d7eabda
Theaceupthesleeve <gamebreaker142007@gmail.com>
2024-10-03 12:27:25 -0700

    edit and delete done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,54 @@
-import { useState } from "react";
+import { useState , useRef } from "react"
 
-function App() {
+const App = () => {
+  const [todo , setTodo] = useState([]);
+  const todoVal = useRef()
 
-  let [num, setNum] = useState(0)
+  const addTodo = (event)=>{
+    event.preventDefault();
+    todo.push(todoVal.current.value)
+    setTodo([...todo]);
+    console.log(todo);
 
-  function minusCounter(){
-    setNum(num - 1)
-    console.log(num);
+    todoVal.current.value = ""
+    
   }
 
-  function addCounter() {
-    // console.log("counter added");
-    setNum(num + 1)
-    console.log(num);
+  const deleteIt = (index)=>{
+    console.log("todo deleted" , index);
+    todo.splice(index , 1);
+    setTodo([...todo]);
+    
+  }
+  const editIt = (index)=>{
+    console.log("todo edited" , index);
+    const editedVal = prompt("enter value");
+    todo.splice(index , 1 , editedVal);
+    setTodo([...todo]);
+    
   }
   return (
     <>
-      <h1>Hello world</h1>
-      <h1>{num}</h1>
-      <button onClick={addCounter}>add </button><br/><br/>
-      <button onClick={minusCounter}>minus </button>
+    <h1>Todo List</h1>
+    <form onSubmit={addTodo}>
+      <input type="text" placeholder="Enter list" ref={todoVal} />
+      <button type="submit">Add</button>
+    </form>
+    <ol>
 
+      {todo.map((item , index)=>{
+        return <div key={index}>
+          <li >{item}</li>
+          <button onClick={()=> editIt(index) }>edit</button>
+          <button onClick={()=> deleteIt(index)}>delete</button>
+        </div>
+      })}
+    </ol>
+    
     </>
   )
+
+  
 }
 
 export default App
